0bea82609d3b76c8017a3852c15848b23bfe7fb7
Adriana <adry_ada@live.com.ar>
2025-04-05 21:50:46 -0300

    Estructura de datos

---
+++ estructuras.py
@@ -0,0 +1,27 @@
+#----------------------------------
+#TIPOS DE DATOS SECUENCIA/COLECCIÓN
+#----------------------------------
+
+
+#LISTA: [COLECCIÓN ORDENADA Y MUTATBLE]
+lista = [1,2,3,4]
+
+#TUPLA: [COLECCIÓN ORDENADA PERO  INMUTATBLE]
+tupla = (1,2,3,4)
+
+#RANGE: [SECUENCIA INMUTATBLE DE NROS]
+rango = range(0,10)
+
+#----------------------------------
+#TIPOS DE DATOS DE MAPEO(MAPPING TYPE)
+#----------------------------------
+
+#DICCIONARIO [COLECCIÓN ORDENADA DE PARES CLAVE-VALOR]
+diccionario ={
+    "nombre": "Ada",
+    "edad": 28,
+    "tecnologías": ["Python", "Java"]
+}
+
+
+
