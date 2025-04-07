1464d5589e2ecdbf35461d62abd06b7a25bf821b
Adriana <adry_ada@live.com.ar>
2025-04-07 17:54:29 -0300

    Eliminando elementos, índices y vaciando lista

--- listas.py
+++ listas.py
@@ -3,18 +3,17 @@
 #----------------------------------
 
 lista = ["Manzana", "Plátano", "Mandarina","Fresa", "Piña"]
-print(lista[-2])
+
 
 """"
+print(lista[-2])
 if "Sandia" in lista:
     print("Si, Sandia está en la lista")
 else:
    print("No, Sandia no está en la lista")
-"""
-""""
+
 lista[4:] = ["Frutilla", "Ananá"]
 print(lista)
-"""
 
 lista.insert(2, "Aguacate")
 print(lista)
@@ -30,4 +29,22 @@ print(lista)
 #Agregando Tuplas
 tupla = ("Melón", "Kiwi")
 lista.extend(tupla)
+print(lista)
+
+
+#Eliminando elementos
+lista.remove("Fresa")
+print(lista)
+
+#Eliminando índice
+lista.pop(2)
+print(lista)
+"""
+print(lista)
+#Usando  pop sin argumento el cual elimina el último elemento
+lista.pop()
+print(lista)
+
+#Limpiar lista (vacia)
+lista.clear()
 print(lista)
