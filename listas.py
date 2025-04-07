6c83f67db44fddfcc3a2709668757929ac51086e
Adriana <adry_ada@live.com.ar>
2025-04-07 17:47:08 -0300

    Agregando Listas y tuplas

--- listas.py
+++ listas.py
@@ -5,7 +5,29 @@
 lista = ["Manzana", "Plátano", "Mandarina","Fresa", "Piña"]
 print(lista[-2])
 
+""""
 if "Sandia" in lista:
     print("Si, Sandia está en la lista")
 else:
    print("No, Sandia no está en la lista")
+"""
+""""
+lista[4:] = ["Frutilla", "Ananá"]
+print(lista)
+"""
+
+lista.insert(2, "Aguacate")
+print(lista)
+lista.append("Limón")
+print(lista)
+
+#Agregando Listas 
+
+lista2 = ["Lechuga", "Tomate", "Zanahoria"]
+lista.extend(lista2)
+print(lista)
+
+#Agregando Tuplas
+tupla = ("Melón", "Kiwi")
+lista.extend(tupla)
+print(lista)
