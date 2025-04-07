26020f882a9f557d27dc3ebd8475497117fcc0df
Adriana <adry_ada@live.com.ar>
2025-04-07 17:19:33 -0300

    Haciendo cambios

--- listas.py
+++ listas.py
@@ -4,3 +4,8 @@
 
 lista = ["Manzana", "Plátano", "Mandarina","Fresa", "Piña"]
 print(lista[-2])
+
+if "Sandia" in lista:
+    print("Si, Sandia está en la lista")
+else:
+   print("No, Sandia no está en la lista")
