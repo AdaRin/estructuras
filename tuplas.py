9e27196de21cf985496b15853826e14a717e9237
Adriana <adry_ada@live.com.ar>
2025-04-16 16:57:01 -0300

    Tuplas

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+#----------------------------------
+#LISTAS [Colección de elementos inmutables y ordenados]
+#----------------------------------
+
+
+
+frutas = ("Manzana", "Plátano", "Cereza", "Manzana")
+
+print(len(frutas))
