0e42b25b26f414057f6e993e36c8703739519f7a
Adriana <adry_ada@live.com.ar>
2025-04-22 19:03:28 -0300

    Conjuntos (Sets)

---
+++ conjuntos.py
@@ -0,0 +1,16 @@
+#----------------------------------
+#CONJUNTOS [Colección no ordenada de elementos únicos y mutables]
+#----------------------------------
+
+
+
+conjunto_frutas = {"Manzana", "Plátano", "Cereza", "Manzana","Banana"}
+
+#conjunto_frutas = set("Manzana")
+print(conjunto_frutas)
+
+
+#ACCEDER, AGREGAR Y ELIMINAR
+
+for fruta in conjunto_frutas:
+    print(fruta)
