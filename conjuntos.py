fd82325281a7aede59a2290b4b1b6e2e2b6c9c2f
Adriana <adry_ada@live.com.ar>
2025-04-23 18:14:39 -0300

    Acceder, agregar Items en Set

--- conjuntos.py
+++ conjuntos.py
@@ -12,5 +12,19 @@ print(conjunto_frutas)
 
 #ACCEDER, AGREGAR Y ELIMINAR
 
+#ACCEDER
 for fruta in conjunto_frutas:
     print(fruta)
+
+print("Banana" in conjunto_frutas)
+
+#AGREGAMOS UN ELEMENTO
+conjunto_frutas.add("Frutilla")
+
+print(conjunto_frutas)
+
+#AGREGAMOS UN SET
+
+conjunto_frutas2 = {"kIWI", "ARANDANOS", "MANGO"}
+conjunto_frutas.update(conjunto_frutas2)
+print(conjunto_frutas)
