b1c6fc5b7afd55da35a920074feaafa955b368d4
Adriana <adry_ada@live.com.ar>
2025-04-24 18:50:05 -0300

    Actualizar, agregar y eliminar elementos

--- diccionario.py
+++ diccionario.py
@@ -26,3 +26,26 @@ if "nombre" in diccionario:
     print("La clave 'nombre' existe en el diccionario.")
 else:   
     print("La clave 'nombre' no existe en el diccionario.")       
+
+#Cambiar, agregar o eliminar elementos
+# update
+# El método update() se utiliza para agregar o actualizar elementos en un diccionario.
+# Si la clave ya existe, se actualiza su valor; si no existe, se agrega un nuevo par clave-valor.
+diccionario.update({"nombre": "Pedro", "apellido": "Gómez"})                    
+
+#borrar
+# El método pop() se utiliza para eliminar un elemento del diccionario utilizando su clave.
+# Devuelve el valor asociado a la clave eliminada.      
+diccionario.pop("apellido")
+print(diccionario)
+
+# El método del se utiliza para eliminar un elemento del diccionario utilizando su clave.
+# No devuelve el valor asociado a la clave eliminada.
+del diccionario["nombre"]           
+print(diccionario)
+
+# El método clear() se utiliza para eliminar todos los elementos del diccionario.
+# Después de llamar a este método, el diccionario estará vacío. 
+diccionario.clear()
+print(diccionario)
+
