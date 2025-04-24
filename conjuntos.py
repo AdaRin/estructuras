f2dc2a0c5d97d9172f0f0f0a13c7a243ae759c3a
Adriana <adry_ada@live.com.ar>
2025-04-24 15:39:40 -0300

    Unión y diferencia

--- conjuntos.py
+++ conjuntos.py
@@ -28,3 +28,31 @@ print(conjunto_frutas)
 conjunto_frutas2 = {"kIWI", "ARANDANOS", "MANGO"}
 conjunto_frutas.update(conjunto_frutas2)
 print(conjunto_frutas)
+
+
+#ELIMINAR ELEMENTOS
+conjunto_frutas.remove("Banana")
+print(conjunto_frutas)
+
+conjunto_frutas.discard("Manzana")
+print(conjunto_frutas)
+
+#JUNTAR SET Y BLUCLES EN CONJUNTOS
+conjunto_letras = {"a", "b", "c"}
+conjunto_numeros = {1, 2, 3}
+
+union = conjunto_letras | conjunto_numeros
+print(union)
+
+#JUNTAR SET Y TUPla
+conjunto_letras.update(conjunto_numeros)
+print(conjunto_letras)
+
+set_tecno_chicago = {"Python", "JavaScript", "AWS"}
+set_tecno_ny = {"Python", "Java", "C++"}
+
+set_tecno_chicago.intersection_update(set_tecno_ny)
+print(set_tecno_chicago)
+
+set_tecno_chicago.difference_update(set_tecno_ny)
+print(set_tecno_chicago)
